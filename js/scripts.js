971c53198300d2a5d97f5a7834796205a0a8c8bf
Nabila Taufiq <nabila.taufiq@verizon.com>
2019-07-17 11:47:41 -0500

    added the files

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+const search = `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+</form>`;
+const div = `<div class="card">
+<div class="card-img-container">
+    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+</div>
+<div class="card-info-container">
+    <h3 id="name" class="card-name cap">first last</h3>
+    <p class="card-text">email</p>
+    <p class="card-text cap">city, state</p>
+</div>
+</div>`;
+
+const modal = `<div class="modal-container">
+<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+</div>`;
+
+$(".search-container").append(search);
+$("gallery").append(div);
+$("body").append(modal);
